d28641823babcaa4f6a4af027710fd0ee919346c
irequ_000 <irequ_000@VAIO-PC>
2016-01-09 12:03:25 +0000

    primera versión con clases mock

---
+++ src/moneycalculator/Application.java
@@ -0,0 +1,22 @@
+package moneycalculator;
+
+import moneycalculator.control.ExchangeCommand;
+import moneycalculator.persistence.MockExchangeRateReader;
+import moneycalculator.ui.MockCurrencyDialog;
+import moneycalculator.ui.MockMoneyDialog;
+import moneycalculator.ui.MockMoneyDisplay;
+
+public class Application {
+
+    public static void main(String[] args) {
+        ExchangeCommand exchangeCommand = new ExchangeCommand(
+                new MockMoneyDialog(),
+                new MockCurrencyDialog(),
+                new MockExchangeRateReader(), 
+                new MockMoneyDisplay()
+        );
+        exchangeCommand.execute();
+                
+    }
+    
+}
